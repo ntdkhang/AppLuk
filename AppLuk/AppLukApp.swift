80da168ea4db6c2d41be04eb1d615c7051783008
ntdkhang <ntdkhang143@gmail.com>
2024-08-02 09:36:06 -0500

    first commit

---
+++ AppLuk/AppLukApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLukApp.swift
+//  AppLuk
+//
+//  Created by Khang Nguyen on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AppLukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
