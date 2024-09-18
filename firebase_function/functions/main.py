32cdc256407cb684417659e7a1c7bdf3aac7b0c4
ntdkhang <ntdkhang143@gmail.com>
2024-09-17 22:15:03 -0500

    notifications for comment

--- firebase_function/functions/main.py
+++ firebase_function/functions/main.py
@@ -4,7 +4,9 @@
 
 from firebase_functions import firestore_fn
 import firebase_admin
-from firebase_admin import db, firestore, messaging
+from firebase_admin import firestore, messaging
+from google.cloud.firestore_v1.base_query import FieldFilter
+
 
 app = firebase_admin.initialize_app()
 
@@ -33,7 +35,7 @@ def addFriend(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -
     token = db.collection("fcmTokens").document(toId).get()
 
     # Query for an opposite friend request
-    docs = db.collection("friendRequests").where("fromId", "==", toId).where("toId", "==", fromId)
+    docs = db.collection("friendRequests").where(filter=FieldFilter("fromId", "==", toId)).where(filter=FieldFilter("toId", "==", fromId))
     count = docs.count().get()[0][0].value
     if count > 0:
         otherDoc = db.collection("users").document(toId)
@@ -55,10 +57,7 @@ def addFriend(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -
                         body=f"{fromUser["name"]} accepted your friend request",
                         )
                 msg = messaging.Message(token=fcm_token, notification=notification)
-                response: messaging.BatchResponse = messaging.send(msg)
-                if response.failure_count < 1:
-                    # Sucess!
-                    return
+                messaging.send(msg)
 
     else:
         # Send notification to say that user sent a friend request
@@ -71,9 +70,58 @@ def addFriend(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -
                         body=f"{fromUser["name"]} sent a friend request",
                         )
                 msg = messaging.Message(token=fcm_token, notification=notification)
-                response: messaging.BatchResponse = messaging.send(msg)
-                if response.failure_count < 1:
-                    # Sucess!
-                    return
+                messaging.send(msg)
+
+
+
+@firestore_fn.on_document_created(document="knowledges/{knowledgeId}/comments/{commentId}")
+def newCommentNoti(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None]) -> None:
+    db = firestore.client(app)
+
+    if event.data is None:
+        return
+    newComment = event.data.to_dict()
+    if newComment is None:
+        return
+    commenterId = newComment["postedById"]
+    commenter = db.collection("users").document(commenterId).get().to_dict()
+
+
+    knowledgeId = event.params["knowledgeId"]
+    knowledge = db.collection("knowledges").document(knowledgeId).get().to_dict()
+    # knowledge = knowledgeRef.to_dict()
+    if knowledge is not None:
+        knowledgeOnwerId = knowledge["postedById"]
+
+        # notify KNOWLEDGE OWNER
+        token = db.collection("fcmTokens").document(knowledgeOnwerId).get()
+        if token.exists:
+            token_dict = token.to_dict()
+            if token_dict is not None and commenterId != knowledgeOnwerId and commenter is not None:
+                fcm_token = token_dict["token"]
+                notification = messaging.Notification(
+                        title="New comment",
+                        body=f"{commenter["name"]} commented on your post",
+                        )
+                msg = messaging.Message(token=fcm_token, notification=notification)
+                messaging.send(msg)
+
+        # Notify all users who commented on this post
+        idsToSend = []
+        comments = db.collection("knowledges").document(knowledgeId).collection("comments").stream()
+        for comment in comments:
+            id = comment.to_dict()["postedById"]
+            if id not in idsToSend and id != knowledgeOnwerId and id != commenterId:
+                token = db.collection("fcmTokens").document(id).get()
+                if token.exists:
+                    token_dict = token.to_dict()
+                    if token_dict is not None and commenter is not None:
+                        fcm_token = token_dict["token"]
+                        notification = messaging.Notification(
+                                title="New comment",
+                                body=f"{commenter["name"]} commented on a post you interacted with",
+                                )
+                        msg = messaging.Message(token=fcm_token, notification=notification)
+                        messaging.send(msg)
 
 
